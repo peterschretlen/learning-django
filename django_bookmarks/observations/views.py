b4ff1cd0063938dffc7bc1bed3855073b6aa38ed
peterschretlen <peter.schretlen@gmail.com>
2015-08-30 22:01:51 -0400

    render view using base bootsrap template

--- django_bookmarks/observations/views.py
+++ django_bookmarks/observations/views.py
@@ -1,9 +1,8 @@
-from django.http import HttpResponse
 from django.views.generic import View
+from django.shortcuts import render
 
 class Index(View):
 	def get(self, request): 
-		return HttpResponse('I am called from a get Request')
-
-	def post(self, request): 
-		return HttpResponse('I am called from a post Request')
+		params = {}
+		params["name"] = "Django"
+		return render(request, 'base.html', params)
